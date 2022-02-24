2fac0d2622dd05e6b0f258582e81a678fa056f09
Aleksej <invinovic@gmail.com>
2022-02-24 18:45:55 +0100

    blue slock

--- slock/config.def.h
+++ slock/config.def.h
@@ -4,9 +4,21 @@ static const char *group = "nogroup";
 
 static const char *colorname[NUMCOLS] = {
 	[INIT] =   "black",     /* after initialization */
-	[INPUT] =  "#282828",   /* during input */
-	[FAILED] = "#d71513",   /* wrong password */
+	[INPUT] =  "#005577",   /* during input */
+	[FAILED] = "#cc3333",   /* wrong password */
 };
 
+/* lock screen opacity */
+static const float alpha = 1;
+
 /* treat a cleared input like a wrong password (color) */
-static const int failonclear = 0;
+static const int failonclear = 1;
+
+/* default message */
+static const char * message = "Suckless: Software that sucks less.";
+
+/* text color */
+static const char * text_color = "#ffffff";
+
+/* text size (must be a valid size) */
+static const char * text_size = "fixed";
--- slock/config.h
+++ slock/config.h
@@ -3,10 +3,22 @@ static const char *user  = "nobody";
 static const char *group = "nogroup";
 
 static const char *colorname[NUMCOLS] = {
-	[INIT] =   "black",     /* after initialization */
+	[INIT] =   "#000000",   /* after initialization */
 	[INPUT] =  "#282828",   /* during input */
 	[FAILED] = "#d71513",   /* wrong password */
 };
 
+/* lock screen opacity */
+static const float alpha = 0.5;
+
 /* treat a cleared input like a wrong password (color) */
-static const int failonclear = 0;
+static const int failonclear = 1;
+
+/* default message */
+static const char * message = "";
+
+/* text color */
+static const char * text_color = "#eeeeee";
+
+/* text size (must be a valid size) */
+static const char * text_size = "fixed";
